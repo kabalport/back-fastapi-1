0082ebea17709e65f3b49314002e9bf0735d509c
kabalport <kabalport@gmail.com>
2023-05-07 17:25:01 +0900

    132

---
+++ ReadMe.md
@@ -0,0 +1,9 @@
+# 설치
+- pip install fastapi
+- pip install pandas
+- pip install requests
+
+# install
+- pandas : 파이썬에서 데이터를 다루는 작업을 할때 아주 유용한 패키지
+- requests : http통신을 위한 패키지
+- implicit : 다양한 머신러닝 기법을 이용해서 추천 엔진을 만들 수 있게 해주는 파이썬 패키지
--- main.py
+++ main.py
@@ -1,16 +1,63 @@
-# This is a sample Python script.
+from distutils.command import config
+from typing import List, Optional
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from fastapi import FastAPI, Query
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware import Middleware
+
+from recommender import item_based_recommendation, user_based_recommendation
+from resolver import random_genres_items, random_items
+
+origins = [
+    # "http://localhost",
+    # "http://localhost:3000",
+    # "https://dq-hustlecoding.github.io/dqflex",
+    # "https://dq-hustlecoding.github.io",
+    # "http://api.dqflex.kro.kr:8080",
+    # "http://api.dqflex.kro.kr",
+    "*",
+]
+
+middleware = [
+    Middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+]
+
+app = FastAPI(middleware=middleware)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@app.get("/all/")
+async def all_movies():
+    result = random_items()
+    return {"result": result}
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+@app.get("/genres/{genre}")
+async def genre_movies(genre: str):
+    result = random_genres_items(genre)
+    return {"result": result}
+
+
+@app.get("/user-based/")
+async def user_based(params: Optional[List[str]] = Query(None)):
+    input_ratings_dict = dict(
+        (int(x.split(":")[0]), float(x.split(":")[1])) for x in params
+    )
+    result = user_based_recommendation(input_ratings_dict)
+    return {"result": result}
+
+
+@app.get("/item-based/{item_id}")
+async def item_based(item_id: str):
+    result = item_based_recommendation(item_id)
+    return {"result": result}
